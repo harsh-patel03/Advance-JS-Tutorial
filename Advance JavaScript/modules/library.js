e26b992f2a90c9948359f7163cb080000bd8d478
harsh-patel03 <hudesai03@gmail.com>
2024-07-16 10:50:53 +0530

    Add files via upload

---
+++ Advance JavaScript/modules/library.js
@@ -0,0 +1,30 @@
+/*
+let message = "ES6 Modules"; 
+
+function user(name){
+    return `Hello ${name}`;
+}
+
+class test{
+    constructor(){
+        console.log("Constructer Method");
+    }
+}
+
+export{ message , user , test };
+*/
+
+
+
+/*
+export default function(){
+    console.log("Harsh Patel");
+}
+*/
+
+
+function user(){
+    return`Hello`;
+}
+
+export{ user };
---
+++ Advance JavaScript/modules/main.js
@@ -0,0 +1,22 @@
+// import { message , user as us , test } from "./library.js";
+
+// import{default as hello} from "./library.js";
+// hello();
+
+
+
+/*
+import * as hello from "./library.js";
+
+console.log(hello.message);
+
+console.log(hello.user("Harsh Patel"));
+
+let a = new hello.test();
+*/
+
+
+
+import { user } from "./bridge.js";
+
+console.log(user());
